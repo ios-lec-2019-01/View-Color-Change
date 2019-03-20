5c579e0a96acc82525e410ce7adaae35e7dfd0c9
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-20 15:40:34 +0900

    alpha  값 추가

--- ViewColorChange/ViewController.swift
+++ ViewColorChange/ViewController.swift
@@ -23,17 +23,20 @@ class ViewController: UIViewController {
 
     @IBAction func bittonRedPressed(_ sender: Any) {
         //redView.backgroundColor = UIColor.white
-        redView.isHidden = true
+//        redView.isHidden = true
+        redView.alpha = 0.5
     }
     
     @IBAction func buttonBluePressed(_ sender: Any) {
         //blueView.backgroundColor = UIColor.white
-        blueView.isHidden = true
+//        blueView.isHidden = true
+        blueView.alpha = 0.5
     }
     
     @IBAction func buttonGreenPressed(_ sender: Any) {
         //greenView.backgroundColor = UIColor.white
-        greenView.isHidden = true
+//        greenView.isHidden = true
+        greenView.alpha = 0.5
     }
     
 }
